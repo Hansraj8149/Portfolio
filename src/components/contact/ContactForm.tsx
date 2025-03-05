a68bfcf3fe32bd280e3b7ff634b2ba447e96b665
Hansraj8149 <hansrajsaini8149@gmail.com>
2025-03-05 09:44:35 +0530

    fix: contact form working

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,141 +1,91 @@
-import { useForm } from "@tanstack/react-form";
-
-import { z } from "zod";
+import { useState } from "react";
 import emailjs from "emailjs-com";
 import clsx from "clsx";
 
 interface ContactFormProps {
   onFormSubmit: () => void;
 }
-interface FormType {
-  name: string;
-  email: string;
-  message: string;
-}
+
 const ContactForm = ({ onFormSubmit }: ContactFormProps) => {
-  const form = useForm({
-    defaultValues: {
-      name: "",
-      email: "",
-      message: "",
-    } as FormType,
-    onSubmit: async ({
-      value,
-    }: {
-      value: { name: string; email: string; message: string };
-    }) => {
-      try {
-        await emailjs.send(
-          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
-          value,
-          process.env.NEXT_PUBLIC_EMAILJS_USER_ID!
-        );
-        onFormSubmit();
-      } catch (error) {
-        console.error("Email send error", error);
-      }
-    },
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    message: "",
   });
+  const [loading, setLoading] = useState(false);
+
+  const handleChangeInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+        e.target as HTMLFormElement,
+        process.env.NEXT_PUBLIC_EMAILJS_USER_ID!
+      )
+      .then(() => onFormSubmit())
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  };
 
   return (
     <form
-      onSubmit={(e) => {
-        const data = new FormData(e.target as HTMLFormElement);
-        data.forEach((value, name) => {
-          if (form.getFieldValue(name as keyof FormType) == "") {
-            form.setFieldValue(name as keyof FormType, value as string);
-          }
-        });
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
       className="p-6 rounded-xl bg-secondary-lighter dark:bg-background-light-dark border border-border dark:border-border-dark-mode hover:bg-secondary-lighter"
+      onSubmit={handleSubmit}
     >
       <h3 className="text-lg font-medium mb-4 dark:text-text-dark">
         Send me a message
       </h3>
+
       <div className="space-y-6">
-        <form.Field
-          name="name"
-          validators={{
-            onBlur: z.string().min(3, "Name must be at least 3 characters"),
-          }}
-        >
-          {(field) => (
-            <input
-              className={clsx(
-                "w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border",
-                field.state.meta.isTouched &&
-                  field.state.meta.errors.length &&
-                  "border-red-500"
-              )}
-              type="text"
-              placeholder="Your Name"
-            />
-          )}
-        </form.Field>
-        <form.Field
+        <input
+          className="w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border"
+          type="text"
+          placeholder="Your Name"
+          name="username"
+          value={formData.username}
+          onChange={handleChangeInput}
+          required
+        />
+
+        <input
+          className="w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border"
+          type="email"
+          placeholder="Your Email"
           name="email"
-          validators={{ onBlur: z.string().email("Invalid email address") }}
-        >
-          {(field) => (
-            <input
-              className={clsx(
-                "w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border",
-                field.state.meta.isTouched &&
-                  field.state.meta.errors.length &&
-                  "border-red-500"
-              )}
-              type="email"
-              placeholder="Your Email"
-            />
-          )}
-        </form.Field>
+          value={formData.email}
+          onChange={handleChangeInput}
+          required
+        />
 
-        <form.Field
+        <textarea
+          className="w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border"
+          placeholder="Your Message"
           name="message"
-          validators={{
-            onBlur: z
-              .string()
-              .min(10, "Message must be at least 10 characters"),
-          }}
-        >
-          {(field) => (
-            <textarea
-              className={clsx(
-                "w-full p-3 rounded-lg bg-background dark:bg-background-dark border border-border",
-                field.state.meta.isTouched &&
-                  field.state.meta.errors.length &&
-                  "border-red-500"
-              )}
-              placeholder="Your Message"
-              rows={5}
-            />
-          )}
-        </form.Field>
+          value={formData.message}
+          onChange={handleChangeInput}
+          rows={5}
+          required
+        />
 
-        <form.Subscribe
-          selector={(state: { isSubmitting: boolean }) => [state.isSubmitting]}
+        <button
+          type="submit"
+          className={clsx(
+            "w-full py-3 rounded-lg font-medium text-secondary-lighter",
+            loading ? "bg-secondary-dark cursor-not-allowed" : "bg-primary"
+          )}
+          disabled={loading}
         >
-          {([isSubmitting]) => {
-            return (
-              <button
-                type="submit"
-                className={clsx(
-                  "w-full py-3 rounded-lg font-medium text-secondary-lighter",
-                  isSubmitting
-                    ? "bg-secondary-dark cursor-not-allowed"
-                    : "bg-primary"
-                )}
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Sending..." : "Send Message"}
-              </button>
-            );
-          }}
-        </form.Subscribe>
+          {loading ? "Sending..." : "Send Message"}
+        </button>
       </div>
     </form>
   );
